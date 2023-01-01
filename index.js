0a5417d68b6f5c63420058cd505876b7da2ca500
HossainAhmed100 <successfulllife101@gmail.com>
2023-01-02 00:06:25 +0600

    ports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 require('dotenv').config();
 const app = express();
 
